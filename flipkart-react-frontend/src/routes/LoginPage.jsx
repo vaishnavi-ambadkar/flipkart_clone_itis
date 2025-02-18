52599e91755f2706ac03624e2a8cb276049bb71f
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-02-18 19:01:31 +0530

    Update LoginPage.jsx

--- flipkart-react-frontend/src/routes/LoginPage.jsx
+++ flipkart-react-frontend/src/routes/LoginPage.jsx
@@ -358,7 +358,7 @@ const LoginPage = () => {
     const loginData = { email, password };
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://flipkart-clone-itis-api.vercel.app/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
