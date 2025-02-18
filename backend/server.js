6c4c273f5498394c7ba2c60566c1bf6ee6219ef7
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-02-18 18:59:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -229,7 +229,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+    origin:[""],
+    methods:["POST","GET"],
+    credentials:true
+    }
+)); 
+
 app.use(bodyParser.json());
 
 // MongoDB Connection
@@ -293,6 +300,10 @@ app.post('/api/place-order', async (req, res) => {
     res.status(400).json({ message: 'Error placing order', error });
   }
 });
+app.get("/",(req,res)=>{
+    res.json("Hello");
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
